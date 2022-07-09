be98686691fb741f3d0f9249997899f5101659ac
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-07-09 12:39:38 +0200

    dispatch keypress

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import Game from "./game";
 import "./index.scss";
 
+const form = document.getElementById("form") as HTMLFormElement;
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+
+  const input = form.elements[0] as HTMLFormElement;
+  const key = input.value;
+
+  window.dispatchEvent(
+    new KeyboardEvent("keydown", {
+      key: key,
+    })
+  );
+});
+
 const game = new Game();
 
 game.play();
