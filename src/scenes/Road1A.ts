242cd59f8e0408a5c40bacc30fb07d268dfc5e39
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-07-10 10:18:02 +0200

    rename Road1 to Road1A

---
+++ src/scenes/Road1A.ts
@@ -0,0 +1,29 @@
+import { println } from "../utils";
+import Scene from "./Scene";
+
+export default class Road1A extends Scene {
+  blueprint(): void {
+    println([
+      "- In front on you the road continues.",
+      "- On your right there's a house",
+      "- On your back there's the village main gate.",
+      "- On your left there's another house.",
+    ]);
+
+    this.move();
+  }
+
+  welcome(): void {
+    println(["You're on the main road."]);
+
+    this.blueprint();
+  }
+
+  right(): void {
+    this.game.scenes.House1.welcome();
+  }
+
+  back(): void {
+    this.game.scenes.MainGate.welcome();
+  }
+}
