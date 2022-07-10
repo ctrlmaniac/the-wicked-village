35169599b7c63121f55b1d12aac8c775209bbb65
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-07-10 11:17:39 +0200

    implements all methods

--- src/scenes/House2Hall.ts
+++ src/scenes/House2Hall.ts
@@ -2,9 +2,37 @@ import { println } from "../utils";
 import Scene from "./Scene";
 
 export default class House2Hall extends Scene {
+  blueprint(): void {
+    println([
+      "- In front of you there's the hallway.",
+      "- On your right there's a closed door.",
+      "- On your back there's the main door to the road.",
+      "- On your left there's the living room.",
+    ]);
+  }
+
   welcome(): void {
     println([
-      "The house is neglected. It seems that noone has lived in this house for a long time.",
+      "The house is neglected.",
+      "It seems that noone has lived in this house for a long time.",
     ]);
+  }
+
+  front(): void {
+    // hallway
+  }
+
+  right(): void {
+    println(["There's a closed door. Do you want to try open it?"]);
+    // diningroom
+  }
+
+  back(): void {
+    println(["You exit the house."]);
+    this.game.scenes.House2.welcome();
+  }
+
+  left(): void {
+    // livingroom
   }
 }
