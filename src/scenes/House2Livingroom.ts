c0ff5d22b2f3e3ebc67aae65c8d3ad6c75db6539
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-07-10 11:23:13 +0200

    init House2Livingroom

---
+++ src/scenes/House2Livingroom.ts
@@ -0,0 +1,23 @@
+import { println } from "../utils";
+import Scene from "./Scene";
+
+export default class House2Livingroom extends Scene {
+  blueprint(): void {
+    println([
+      "- In front of you there's a couch.",
+      "- On your left there are some paintings",
+      "- On your back there's the door to the hall",
+      "- On your left there's a shelf",
+    ]);
+
+    this.move();
+  }
+
+  welcome(): void {
+    println([
+      "The livingroom is a total mess.",
+      "You try to walk but the furniture obstruct the way.",
+    ]);
+    this.blueprint();
+  }
+}
